c2278a3f01e29b14bed6d99e6f48f81292bfe64b
Andrewsorokin13 <Andrewsorokin13@list.ru>
2025-01-04 01:50:29 +0300

    add SettingsViewControllerDelegate

--- ColorFlow/ViewController/SettingsViewController.swift
+++ ColorFlow/ViewController/SettingsViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-final class ViewController: UIViewController {
+final class SettingsViewController: UIViewController {
     
     // MARK: - IB Outlets
     @IBOutlet weak var redLabel: UILabel!
